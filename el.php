88050de56c9bc1fb424ed11970aa2235c91e5809
Dimitris Demiris <ddemiris@gmail.com>
2016-03-23 23:26:51 +0200

    Create el.php

---
+++ el.php
@@ -0,0 +1,25 @@
+<?php
+ 
+$lang = array(
+  "min" => "λεπτά",
+  "for" => "για",
+	"Date" => "Ημερομηνία",
+  "Provide a search criteria" => "Δώστε κριτήριο αναζήτησης για ανεύρεση.",
+	"Search Term" => "Κριτήριο",
+  "Contact" => "Επαφή",
+	"Contacts" => "Επαφές",
+	"Choose" => "Επιλέξτε",
+	"Search for contacts phone records" => "Ανεύρεση Τηλεφωνικών Κλήσεων Επαφών",
+	"For employee" => "H κλήση αφορά",
+	"Anybody" => "Οποιοσδήποτε",
+	"Date Type / Comparison" => "Είδος Ημερομηνίας / Σχέση",
+	"Call" => "Κλήσης",
+	"Answer" => "Απάντησης",
+	"New File" => "Προσθήκη Αρχείου",
+	"Less or Equal" => "Μικρότερη η Ίση",
+	"Less" => "Μικρότερη",
+	"Equal" => "Ίση",
+	"Greater or Equal" => "Μεγαλύτερη ή Ίση",
+	"Personal Shortcuts" => "Προσωπικές Συντομεύσεις",
+	
+	);
