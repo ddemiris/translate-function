f69744ca04de06107219325916965f84b05b598a
Dimitris Demiris <ddemiris@gmail.com>
2016-03-23 23:25:56 +0200

    Create en.php

---
+++ en.php
@@ -0,0 +1,24 @@
+
+<?php
+
+$lang = array(
+  "min" => "min",
+	"for" => "for",
+  "Date" => "Date",
+  "Provide a search criteria" => "Provide a search criteria",
+	"Search Term" => "Search Term",
+	"Contact" => "Contact",
+	"Contacts" => "Contacts",
+	"Choose" => "Choose",
+	"Search for contacts phone records" => "Search for contacts phone records",
+	"For employee" => "For employee",
+	"Anybody" => "Anybody",
+	"Date Type / Comparison" => "Date Type / Comparison",
+	"Call" => "Call",
+	"Answer" => "Answer",
+	"New File" => "New File",
+	"Less or Equal" => "Less or Equal",
+	"Manage your shortcuts" => "Manage your shortcuts",	
+	
+	);
+?>
